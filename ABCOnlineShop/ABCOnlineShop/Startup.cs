adf76653a3fe43a3bbebcc06a3250e4a99533df4
Thato <novemberpatrick97@gmail.com>
2020-10-10 21:34:44 +0200

    Initial Solution Setup

---
+++ ABCOnlineShop/ABCOnlineShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ABCOnlineShop.Startup))]
+namespace ABCOnlineShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
